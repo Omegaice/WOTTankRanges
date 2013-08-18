6d7266e97a2ac40e73751ff0cf2901cf91a2e764
James Sweet <james.sweet88@googlemail.com>
2013-08-18 18:13:12 -0400

    Added building a zip file or the mod

--- compiler.py
+++ compiler.py
@@ -1,3 +1,7 @@
-import py_compile
+import py_compile, zipfile
 
 py_compile.compile("res_mod/8.7/script/client/currentvehicle.py")
+
+fZip = zipfile.ZipFile( "XVMTankRange.zip", "w" )
+fZip.write("res_mod/8.7/script/client/currentvehicle.pyc")
+fZip.close()
