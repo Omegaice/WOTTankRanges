81583d45363ecc6ca5a428dfe62ea28d3596d04e
James Sweet <james.sweet88@googlemail.com>
2013-08-18 20:00:03 -0400

    Corrected mod path

--- compiler.py
+++ compiler.py
@@ -1,7 +1,7 @@
 import py_compile, zipfile
 
-py_compile.compile("res_mod/8.7/script/client/currentvehicle.py")
+py_compile.compile("res_mod/8.7/scripts/client/currentvehicle.py")
 
 fZip = zipfile.ZipFile( "XVMTankRange.zip", "w" )
-fZip.write("res_mod/8.7/script/client/currentvehicle.pyc")
+fZip.write("res_mod/8.7/scripts/client/currentvehicle.pyc")
 fZip.close()
